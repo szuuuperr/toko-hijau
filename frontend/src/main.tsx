f8a1d14951f8bc28c99799f22b2e4f3590934f4e
szuuuperr <adyatma.yafi19@gmail.com>
2026-01-22 12:47:31 +0700

    revert

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ReactGA from "react-ga4";
-
-const GA_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
-
-if (GA_ID) {
-  ReactGA.initialize(GA_ID);
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
